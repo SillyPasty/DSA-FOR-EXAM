691eed3264dcdb7c99961873ab388143795cb35d
SillyPasty <1179041704@qq.com>
2019-12-25 15:26:14 +0800

    改了一个小错误

--- Stack/BalancingSymbols.cpp
+++ Stack/BalancingSymbols.cpp
@@ -12,12 +12,14 @@ int main()
     char str[20];
     bool flag = true;
     cin >> str;
-    for(int i = 0; i < strlen(str); i++)
-        if(str[i] == '(' || str[i] == '[' || str[i] == '{')
+    for (int i = 0; i < strlen(str); i++)
+        if (str[i] == '(' || str[i] == '[' || str[i] == '{')
             s.push(str[i]);
-        else if (str[i] == s.top())
+        else if ((str[i] == '(' && s.top() == ')') ||
+                 (str[i] == '[' && s.top() == ']') ||
+                 (str[i] == '{' && s.top() == '}'))
             s.pop();
-        else if (str[i] != s.top())
+        else
             flag = false;
     printf("%d\n", flag ? "YES" : "NO");
     system("pause");
